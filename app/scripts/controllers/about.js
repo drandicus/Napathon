11e62b0d589d92d7d8f837b88c6ee0755de0ab7f
drandicus <deverasd@gmail.com>
2014-04-09 17:48:00 -0500

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('napathonApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
